90cf09ed464ad952ee22d1320c6cb2246bf14717
HoaDam2002 <damvanhoa305202@gmail.com>
2023-09-09 15:02:01 +0700

    page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('frontend.user.home.home');
+    return view('frontend.pages.home.home');
 });
 
 Auth::routes();
